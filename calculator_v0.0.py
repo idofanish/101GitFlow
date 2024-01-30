3431c1568957a95a6e719b789b6c7e5bd56d9ed9
anish.abraham <idofanish@gmail.com>
2024-01-30 22:07:18 +0530

    Created base template file for Calculator

---
+++ calculator_v0.0.py
@@ -0,0 +1,17 @@
+#Calculator application code starts
+##-------------------------------##
+#Section 1.1 
+ #Addition code starts below
+ #Addition code ends above
+#Section 1.2
+ #Subtraction code starts below
+ #Subtraction code ends above
+#Section 1.3 
+ #Multiplication code starts below
+ #Multiplication code ends above
+#Section 1.4 
+ #Division code starts below
+ #Division code ends above
+ 
+#Calculator application code ends
+##-------------------------------##
