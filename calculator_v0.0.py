fa5c0fe8f3710c618f6e2f4c1bb96bd3113197e3
anish.abraham <idofanish@gmail.com>
2024-01-31 08:51:48 +0530

    Created the basic structure of the code

--- calculator_v0.0.py
+++ calculator_v0.0.py
@@ -1,5 +1,14 @@
 #Calculator application code starts
 ##-------------------------------##
+#Calculator perform a mathmatical operation of two input numbers the user is giving
+#User has to provide 3 inputs -two operands and an operator
+declare x,y,operator;
+x=0;y=0;operator='';
+print("Enter two numbers");
+input(x);
+input(y);
+print("Enter the operator")
+input(operator);
 #Section 1.1 
  #Addition code starts below
  #Addition code ends above
